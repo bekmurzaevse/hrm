c188ac5ca4ff5b11585002a55e654dad547779f3
bekmurzaevse <bekmurzaevse@gmail.com>
2025-07-05 18:23:27 +0500

    created api file

--- routes/web.php
+++ routes/web.php
@@ -1,12 +1,8 @@
 <?php
 
-use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-Cache::remember('test', 600, function () {
-    return "TEST value";
-});
-    // Cache::put('test', 'hello redis', 600);
-    return view('welcome');
+    // return view('welcome');
+    return "HOME";
 });
