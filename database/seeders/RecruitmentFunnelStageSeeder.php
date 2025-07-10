015b0c671851ee3e5a9fc949ace516e12d495780
simpledevweb <ibraymovalpamis81@gmail.com>
2025-07-10 12:00:40 +0500

    feat: Created model, migartion, seeder

---
+++ database/seeders/RecruitmentFunnelStageSeeder.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class RecruitmentFunnelStageSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+    }
+}
